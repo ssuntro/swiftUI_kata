03cf1f24f41b21f9460d649c10f1a596c4ca952a
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-01-08 20:39:48 +0700

    [snake-game] initial project

---
+++ snake_game/snake_game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  snake_game
+//
+//  Created by Siroratt Suntronsuk on 8/1/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ snake_game/snake_game/snake_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  snake_gameApp.swift
+//  snake_game
+//
+//  Created by Siroratt Suntronsuk on 8/1/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct snake_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
