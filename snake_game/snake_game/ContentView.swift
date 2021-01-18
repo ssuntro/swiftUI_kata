2708b246f920bcf7a01530b380bf9251357470a9
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-01-18 23:54:47 +0700

    [UIViewControllerRepresentable] clean unused code

--- snake_game/snake_game/ContentView.swift
+++ snake_game/snake_game/ContentView.swift
@@ -9,31 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CircleNumberView(color: .red, number: 1)
-        Text("Hello, world!")
-            .padding()
+        Text("")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-
-struct CircleNumberView: View {
-    var color: Color
-    var number:Int
-    
-    var body: some View {
-        ZStack {
-            Circle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(color)
-            Text("\(number)")
-                .foregroundColor(.white)
-                .font(.system(size: 70, weight: .bold))
-        }
     }
 }
