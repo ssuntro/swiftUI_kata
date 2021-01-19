e1638d30909ff274f6cb7c13f6c886d2354b3c7c
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-01-19 22:19:23 +0700

    [UIViewControllerRepresentable] assign UIViewControllerDelegate with swiftuiWrapper coordinator

--- snake_game/snake_game/ImagePicker.swift
+++ snake_game/snake_game/ImagePicker.swift
@@ -7,8 +7,17 @@
 
 import SwiftUI
 struct ImagePicker: UIViewControllerRepresentable {
+    class Coordinator: NSObject,
+                       UIImagePickerControllerDelegate,
+                       UINavigationControllerDelegate {
+        
+    }
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
         return picker
     }
     
